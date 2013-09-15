2581cb29270c8693550ef4c3a6c9d13f2ca72962
candycastillo <gold_horus@hotmail.com>
2013-09-14 20:44:04 -0500

    COMMIT TAREA CONVERTIDOR

---
+++ cambio.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+</head>
+
+<body>
+
+<?php echo "Convertidor"; echo "</br>";
+
+$dolares= 10;
+
+$tasaCambio = 15.50; 
+
+$pesos = $dolares * $tasaCambio;
+ 
+echo "Cantidad en pesos es:  "."$". $pesos;
+//Castillo Moreno Candy Claudia 
+?>
+</body>
+
+</html>
